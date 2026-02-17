8339d8a40a9a59c3c2cc13c80bf64ba2f5987b09
Tejashri Tagad <tagadteju45@gmail.com>
2026-02-17 17:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+const form=document.querySelector(".form")
+const loginbtn = document.querySelector("#existing");
+const saveUser=JSON.parse(localStorage.getItem("user"));
+
+document.querySelector("#username").value = "";
+document.querySelector("#password").value = "";
+
+if(saveUser){
+	loginbtn.style.display="block"
+}else{
+	loginbtn.style.display="none"
+}
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	
+	const name=document.querySelector("#username").value
+	const password=document.querySelector("#password").value
+	const checkbox=document.querySelector("#checkbox")
+
+         alert(`Logged in as ${name}`);
+
+	
+       if (checkbox.checked){
+	         const userData={
+		      name:name,
+	           password:password
+	        }
+	         localStorage.setItem("user",JSON.stringify(userData))
+		      loginbtn.style.display="block"
+             console.log(userData);
+            }else{
+		   localStorage.removeItem("user");
+		   loginbtn.style.display="none"
+			}
+        })
+loginbtn.addEventListener("click", () => {
+  const saved = JSON.parse(localStorage.getItem("user"));
+  if (saved) {
+    alert(`Logged in as ${saved.name}`);
+  }
+});
